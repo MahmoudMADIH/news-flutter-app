1ed858f2fab0106e01ed53f5093d9247af5fa06c
Mahmoud_Madih <moodbeder150@gmail.com>
2024-04-01 22:45:31 +0200

    design API for the project

---
+++ lib/homeScreen/homepage.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:newsapp/app_maneger.dart';
+import 'package:newsapp/model/SoursesResponse.dart';
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+static String Route_Name='HomeScreen';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Result> result=[];
+  @override
+  void initState(){
+    //todo: implement initial State
+    super.initState();
+    APImanger.getSoureses().then((soursesResponse) =>
+    result=soursesResponse.results!);
+  }
+  @override
+  Widget build(BuildContext context)  {
+    return Container();
+  }
+}
---
+++ lib/model/SoursesResponse.dart
@@ -0,0 +1,263 @@
+/// status : "success"
+/// totalResults : 725
+/// results : [{"article_id":"956f5c909e0216af76de67fef61bc5db","title":"رئيس الوزراء يستعرض مبادرة اتحاد الدواجن لخفض الأسعار بنسبة 10 - 15%","link":"https://www.shorouknews.com/news/view.aspx?cdate=01042024&id=07d04057-ee20-4a18-b559-5406c1b96675","keywords":null,"creator":["مصر"],"video_url":null,"description":"استعرض الدكتور مصطفى مدبولي، رئيس مجلس الوزراء، تقريرًا أعدّه السيد القصير، وزير الزراعة واستصلاح الأراضي","content":"ONLY AVAILABLE IN PAID PLANS","pubDate":"2024-04-01 08:09:21","image_url":"https://www.shorouknews.com/uploadedimages/Other/original/375470772_766153182221771_8195630518242872116_n.jpg","source_id":"shorouknews","source_url":"https://www.shorouknews.com","source_icon":"https://i.bytvi.com/domain_icons/shorouknews.png","source_priority":436620,"country":["egypt"],"category":["top"],"language":"arabic","ai_tag":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment_stats":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","ai_region":"ONLY AVAILABLE IN CORPORATE PLANS"},{"article_id":"68244afcde98996252427d2b52ce8369","title":"الجيش الإسرائيلي يعلن مقتل أحد جنوده خلال القتال جنوب قطاع غزة","link":"https://www.shorouknews.com/news/view.aspx?cdate=01042024&id=ecf50fb1-694d-422e-b1f8-e17c8544f8c4","keywords":null,"creator":["شرق أوسط"],"video_url":null,"description":"أعلن المتحدث باسم الجيش الإسرائيلي، مقتل الرقيب نداف كوهين /20 عاما/ أثناء القتال جنوب قطاع غزة.","content":"ONLY AVAILABLE IN PAID PLANS","pubDate":"2024-04-01 08:08:40","image_url":"https://www.shorouknews.com/uploadedimages/Other/original/yk13747403-0-0-849-510-0-x-large-1704450532.jpg","source_id":"shorouknews","source_url":"https://www.shorouknews.com","source_icon":"https://i.bytvi.com/domain_icons/shorouknews.png","source_priority":436620,"country":["egypt"],"category":["top"],"language":"arabic","ai_tag":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment_stats":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","ai_region":"ONLY AVAILABLE IN CORPORATE PLANS"},{"article_id":"26a4a7e5f10c2ba5a07ae2a30836889b","title":"Armed Forces Conduct Humanitarian Airdrops To Gaza","link":"https://menafn.com/1108043556/Armed-Forces-Conduct-Humanitarian-Airdrops-To-Gaza","keywords":null,"creator":["menafn.com"],"video_url":null,"description":"(MENAFN - Jordan News Agency) Amman, Apr. 1 (Petra) - The Jordan Armed Forces - Arab Army, conducted Monday 8 humanitarian and food aid airdrops in various locations in the northern Gaza Strip, ...","content":"ONLY AVAILABLE IN PAID PLANS","pubDate":"2024-04-01 08:03:00","image_url":"https://menafn.com/images/Menafn_SmallLogo.jpg","source_id":"menafn","source_url":"https://menafn.com","source_icon":"https://i.bytvi.com/domain_icons/menafn.png","source_priority":1117534,"country":["egypt"],"category":["top"],"language":"english","ai_tag":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment_stats":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","ai_region":"ONLY AVAILABLE IN CORPORATE PLANS"},{"article_id":"f97977dc557e428c637637b7f433471e","title":"قوات الاحتلال الإسرائيلي تعتقل شقيقة إسماعيل هنية","link":"https://www.shorouknews.com/news/view.aspx?cdate=01042024&id=10472c2b-e914-426a-89dc-432b6e668345","keywords":null,"creator":["شرق أوسط"],"video_url":null,"description":"اعتقلت قوات الاحتلال الإسرائيلي، صباح اليوم الاثنين، شقيقة رئيس المكتب السياسي لحركة حماس إسماعيل هنية.","content":"ONLY AVAILABLE IN PAID PLANS","pubDate":"2024-04-01 07:55:00","image_url":"https://www.shorouknews.com/uploadedimages/Other/original/gkdme26xoaafqc4-1711953500.jpg","source_id":"shorouknews","source_url":"https://www.shorouknews.com","source_icon":"https://i.bytvi.com/domain_icons/shorouknews.png","source_priority":436620,"country":["egypt"],"category":["top"],"language":"arabic","ai_tag":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment_stats":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","ai_region":"ONLY AVAILABLE IN CORPORATE PLANS"},{"article_id":"200a3f97d807be131fa46e57c37890b4","title":"تعليق منشورات تهديدية.. قوات الاحتلال تقتحم مخيم شعفاط في القدس برفقة كلاب بوليسية","link":"https://www.shorouknews.com/news/view.aspx?cdate=01042024&id=b77e3841-aaf9-4191-949b-c09e889fff52","keywords":null,"creator":["شرق أوسط"],"video_url":null,"description":"نفذت قوات الاحتلال، اليوم الإثنين، اقتحامات متكررة لمخيم شعفاط في مدينة القدس.","content":"ONLY AVAILABLE IN PAID PLANS","pubDate":"2024-04-01 07:53:24","image_url":"https://www.shorouknews.com/uploadedimages/Other/original/Screenshot 2024-04-01 095306.jpg","source_id":"shorouknews","source_url":"https://www.shorouknews.com","source_icon":"https://i.bytvi.com/domain_icons/shorouknews.png","source_priority":436620,"country":["egypt"],"category":["top"],"language":"arabic","ai_tag":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment_stats":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","ai_region":"ONLY AVAILABLE IN CORPORATE PLANS"},{"article_id":"801fc302f98db943b453ff5b20ceaadc","title":"اختتام مسيرات عيد الفصح الداعية للسلام في ألمانيا اليوم","link":"https://www.shorouknews.com/news/view.aspx?cdate=01042024&id=3db99d5a-b1f4-45b6-b5e4-191a1e509c30","keywords":null,"creator":["العالم"],"video_url":null,"description":"تنتهي مسيرات عيد الفصح التقليدية الداعية للسلام اليوم الاثنين بفعاليات في عدة مدن ألمانية.","content":"ONLY AVAILABLE IN PAID PLANS","pubDate":"2024-04-01 07:50:22","image_url":"https://www.shorouknews.com/uploadedimages/Other/original/فصحححححححححححححححححححححححححححححححح.JPG","source_id":"shorouknews","source_url":"https://www.shorouknews.com","source_icon":"https://i.bytvi.com/domain_icons/shorouknews.png","source_priority":436620,"country":["egypt"],"category":["top"],"language":"arabic","ai_tag":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment_stats":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","ai_region":"ONLY AVAILABLE IN CORPORATE PLANS"},{"article_id":"63f601e8005b3d62ce86d932d4a7d9d9","title":"ماذا حدث بعد انسحاب القوات الإسرائيلية من مستشفى الشفاء ومحيطها في قطاع غزة؟","link":"https://www.shorouknews.com/news/view.aspx?cdate=01042024&id=aa1cd1b6-0653-43f2-9fc9-229e904d46fc","keywords":null,"creator":["شرق أوسط"],"video_url":null,"description":"أفادت تقارير إعلامية بانسحاب القوات الإسرائيلية من مستشفى الشفاء ومحيطها في قطاع غزة بعد أسبوعين من اقتحامه","content":"ONLY AVAILABLE IN PAID PLANS","pubDate":"2024-04-01 07:49:11","image_url":"https://www.shorouknews.com/uploadedimages/Other/original/Screenshot 2024-04-01 094757.jpg","source_id":"shorouknews","source_url":"https://www.shorouknews.com","source_icon":"https://i.bytvi.com/domain_icons/shorouknews.png","source_priority":436620,"country":["egypt"],"category":["top"],"language":"arabic","ai_tag":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment_stats":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","ai_region":"ONLY AVAILABLE IN CORPORATE PLANS"},{"article_id":"b982084c9e4c0d7d61b8de344d489397","title":"مدخنو الحشيش يتجمعون عند بوابة براندنبورج في برلين للاحتفال بتقنين القنب","link":"https://www.shorouknews.com/news/view.aspx?cdate=01042024&id=008a4840-0973-4d75-ad23-cebf5b9dccaf","keywords":null,"creator":["العالم"],"video_url":null,"description":"تجمهر حشد من الأشخاص أمام بوابة براندنبورج التاريخية في برلين بعد منتصف الليل مباشرة وهم يدخنون لفائف من القنب (الحشيش)، وذلك احتفالا","content":"ONLY AVAILABLE IN PAID PLANS","pubDate":"2024-04-01 07:43:45","image_url":"https://www.shorouknews.com/uploadedimages/Other/original/ححششيشششششششششششششششششششششششششششششششششششششششششش.JPG","source_id":"shorouknews","source_url":"https://www.shorouknews.com","source_icon":"https://i.bytvi.com/domain_icons/shorouknews.png","source_priority":436620,"country":["egypt"],"category":["top"],"language":"arabic","ai_tag":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment_stats":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","ai_region":"ONLY AVAILABLE IN CORPORATE PLANS"},{"article_id":"9f82c58661b0b251884d47d59d98906e","title":"كوريا الجنوبية تعتزم استثمار 13.1 مليار دولار في صناعة أدوية التكنولوجيا الحيوية بحلول 2030","link":"https://www.shorouknews.com/news/view.aspx?cdate=01042024&id=c059e003-8f51-4e34-baa0-eb53c6d99225","keywords":null,"creator":["العالم"],"video_url":null,"description":"أعلنت وزارة التجارة والصناعة الكورية الجنوبية، اليوم الاثنين، اعتزام الحكومة إنفاق 17.7 تريليون وون (13.12 مليار دولار) للاستثمار في","content":"ONLY AVAILABLE IN PAID PLANS","pubDate":"2024-04-01 07:41:29","image_url":"https://www.shorouknews.com/uploadedimages/Box/original/صناعة أدوية التكنولوجيا الحيوية.jpg","source_id":"shorouknews","source_url":"https://www.shorouknews.com","source_icon":"https://i.bytvi.com/domain_icons/shorouknews.png","source_priority":436620,"country":["egypt"],"category":["top"],"language":"arabic","ai_tag":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment_stats":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","ai_region":"ONLY AVAILABLE IN CORPORATE PLANS"},{"article_id":"306d155a03b7d9b0d5c156c97f2060c3","title":"بريطانيا: توقعات بهطول أمطار غزيرة في جنوب إنجلترا اليوم","link":"https://www.shorouknews.com/news/view.aspx?cdate=01042024&id=1ced51ac-a924-4cd5-9f63-d2647fb1fa18","keywords":null,"creator":["العالم"],"video_url":null,"description":"توقع خبراء الأرصاد هطول أمطار غزيرة في جنوب إنجلترا اليوم الاثنين، وسط عطلة نهاية أسبوع طويلة بمناسبة عيد الفصح.","content":"ONLY AVAILABLE IN PAID PLANS","pubDate":"2024-04-01 07:40:34","image_url":"https://www.shorouknews.com/uploadedimages/Other/original/ااااممطططططططططططططططططططططططططططططططططططططططططططط.JPG","source_id":"shorouknews","source_url":"https://www.shorouknews.com","source_icon":"https://i.bytvi.com/domain_icons/shorouknews.png","source_priority":436620,"country":["egypt"],"category":["top"],"language":"arabic","ai_tag":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","sentiment_stats":"ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS","ai_region":"ONLY AVAILABLE IN CORPORATE PLANS"}]
+/// nextPage : "1711957234041580285"
+
+class SoursesResponse {
+  SoursesResponse({
+      String? status, 
+      num? totalResults, 
+      List<Result>? results,
+      String? nextPage,}){
+    _status = status;
+    _totalResults = totalResults;
+    _results = results;
+    _nextPage = nextPage;
+}
+
+  SoursesResponse.fromJson(dynamic json) {
+    _status = json['status'];
+    _totalResults = json['totalResults'];
+    if (json['results'] != null) {
+      _results = [];
+      json['results'].forEach((v) {
+        _results?.add(Result.fromJson(v));
+      });
+    }
+    _nextPage = json['nextPage'];
+  }
+  String? _status;
+  num? _totalResults;
+  List<Result>? _results;
+  String? _nextPage;
+SoursesResponse copyWith({  String? status,
+  num? totalResults,
+  List<Result>? results,
+  String? nextPage,
+}) => SoursesResponse(  status: status ?? _status,
+  totalResults: totalResults ?? _totalResults,
+  results: results ?? _results,
+  nextPage: nextPage ?? _nextPage,
+);
+  String? get status => _status;
+  num? get totalResults => _totalResults;
+  List<Result>? get results => _results;
+  String? get nextPage => _nextPage;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['status'] = _status;
+    map['totalResults'] = _totalResults;
+    if (_results != null) {
+      map['results'] = _results?.map((v) => v.toJson()).toList();
+    }
+    map['nextPage'] = _nextPage;
+    return map;
+  }
+
+}
+
+/// article_id : "956f5c909e0216af76de67fef61bc5db"
+/// title : "رئيس الوزراء يستعرض مبادرة اتحاد الدواجن لخفض الأسعار بنسبة 10 - 15%"
+/// link : "https://www.shorouknews.com/news/view.aspx?cdate=01042024&id=07d04057-ee20-4a18-b559-5406c1b96675"
+/// keywords : null
+/// creator : ["مصر"]
+/// video_url : null
+/// description : "استعرض الدكتور مصطفى مدبولي، رئيس مجلس الوزراء، تقريرًا أعدّه السيد القصير، وزير الزراعة واستصلاح الأراضي"
+/// content : "ONLY AVAILABLE IN PAID PLANS"
+/// pubDate : "2024-04-01 08:09:21"
+/// image_url : "https://www.shorouknews.com/uploadedimages/Other/original/375470772_766153182221771_8195630518242872116_n.jpg"
+/// source_id : "shorouknews"
+/// source_url : "https://www.shorouknews.com"
+/// source_icon : "https://i.bytvi.com/domain_icons/shorouknews.png"
+/// source_priority : 436620
+/// country : ["egypt"]
+/// category : ["top"]
+/// language : "arabic"
+/// ai_tag : "ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS"
+/// sentiment : "ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS"
+/// sentiment_stats : "ONLY AVAILABLE IN PROFESSIONAL AND CORPORATE PLANS"
+/// ai_region : "ONLY AVAILABLE IN CORPORATE PLANS"
+
+class Result {
+  Result({
+      String? articleId, 
+      String? title, 
+      String? link, 
+      dynamic keywords, 
+      List<String>? creator, 
+      dynamic videoUrl, 
+      String? description, 
+      String? content, 
+      String? pubDate, 
+      String? imageUrl, 
+      String? sourceId, 
+      String? sourceUrl, 
+      String? sourceIcon, 
+      num? sourcePriority, 
+      List<String>? country, 
+      List<String>? category, 
+      String? language, 
+      String? aiTag, 
+      String? sentiment, 
+      String? sentimentStats, 
+      String? aiRegion,}){
+    _articleId = articleId;
+    _title = title;
+    _link = link;
+    _keywords = keywords;
+    _creator = creator;
+    _videoUrl = videoUrl;
+    _description = description;
+    _content = content;
+    _pubDate = pubDate;
+    _imageUrl = imageUrl;
+    _sourceId = sourceId;
+    _sourceUrl = sourceUrl;
+    _sourceIcon = sourceIcon;
+    _sourcePriority = sourcePriority;
+    _country = country;
+    _category = category;
+    _language = language;
+    _aiTag = aiTag;
+    _sentiment = sentiment;
+    _sentimentStats = sentimentStats;
+    _aiRegion = aiRegion;
+}
+
+  Result.fromJson(dynamic json) {
+    _articleId = json['article_id'];
+    _title = json['title'];
+    _link = json['link'];
+    _keywords = json['keywords'];
+    _creator = json['creator'] != null ? json['creator'].cast<String>() : [];
+    _videoUrl = json['video_url'];
+    _description = json['description'];
+    _content = json['content'];
+    _pubDate = json['pubDate'];
+    _imageUrl = json['image_url'];
+    _sourceId = json['source_id'];
+    _sourceUrl = json['source_url'];
+    _sourceIcon = json['source_icon'];
+    _sourcePriority = json['source_priority'];
+    _country = json['country'] != null ? json['country'].cast<String>() : [];
+    _category = json['category'] != null ? json['category'].cast<String>() : [];
+    _language = json['language'];
+    _aiTag = json['ai_tag'];
+    _sentiment = json['sentiment'];
+    _sentimentStats = json['sentiment_stats'];
+    _aiRegion = json['ai_region'];
+  }
+  String? _articleId;
+  String? _title;
+  String? _link;
+  dynamic _keywords;
+  List<String>? _creator;
+  dynamic _videoUrl;
+  String? _description;
+  String? _content;
+  String? _pubDate;
+  String? _imageUrl;
+  String? _sourceId;
+  String? _sourceUrl;
+  String? _sourceIcon;
+  num? _sourcePriority;
+  List<String>? _country;
+  List<String>? _category;
+  String? _language;
+  String? _aiTag;
+  String? _sentiment;
+  String? _sentimentStats;
+  String? _aiRegion;
+Result copyWith({  String? articleId,
+  String? title,
+  String? link,
+  dynamic keywords,
+  List<String>? creator,
+  dynamic videoUrl,
+  String? description,
+  String? content,
+  String? pubDate,
+  String? imageUrl,
+  String? sourceId,
+  String? sourceUrl,
+  String? sourceIcon,
+  num? sourcePriority,
+  List<String>? country,
+  List<String>? category,
+  String? language,
+  String? aiTag,
+  String? sentiment,
+  String? sentimentStats,
+  String? aiRegion,
+}) => Result(  articleId: articleId ?? _articleId,
+  title: title ?? _title,
+  link: link ?? _link,
+  keywords: keywords ?? _keywords,
+  creator: creator ?? _creator,
+  videoUrl: videoUrl ?? _videoUrl,
+  description: description ?? _description,
+  content: content ?? _content,
+  pubDate: pubDate ?? _pubDate,
+  imageUrl: imageUrl ?? _imageUrl,
+  sourceId: sourceId ?? _sourceId,
+  sourceUrl: sourceUrl ?? _sourceUrl,
+  sourceIcon: sourceIcon ?? _sourceIcon,
+  sourcePriority: sourcePriority ?? _sourcePriority,
+  country: country ?? _country,
+  category: category ?? _category,
+  language: language ?? _language,
+  aiTag: aiTag ?? _aiTag,
+  sentiment: sentiment ?? _sentiment,
+  sentimentStats: sentimentStats ?? _sentimentStats,
+  aiRegion: aiRegion ?? _aiRegion,
+);
+  String? get articleId => _articleId;
+  String? get title => _title;
+  String? get link => _link;
+  dynamic get keywords => _keywords;
+  List<String>? get creator => _creator;
+  dynamic get videoUrl => _videoUrl;
+  String? get description => _description;
+  String? get content => _content;
+  String? get pubDate => _pubDate;
+  String? get imageUrl => _imageUrl;
+  String? get sourceId => _sourceId;
+  String? get sourceUrl => _sourceUrl;
+  String? get sourceIcon => _sourceIcon;
+  num? get sourcePriority => _sourcePriority;
+  List<String>? get country => _country;
+  List<String>? get category => _category;
+  String? get language => _language;
+  String? get aiTag => _aiTag;
+  String? get sentiment => _sentiment;
+  String? get sentimentStats => _sentimentStats;
+  String? get aiRegion => _aiRegion;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['article_id'] = _articleId;
+    map['title'] = _title;
+    map['link'] = _link;
+    map['keywords'] = _keywords;
+    map['creator'] = _creator;
+    map['video_url'] = _videoUrl;
+    map['description'] = _description;
+    map['content'] = _content;
+    map['pubDate'] = _pubDate;
+    map['image_url'] = _imageUrl;
+    map['source_id'] = _sourceId;
+    map['source_url'] = _sourceUrl;
+    map['source_icon'] = _sourceIcon;
+    map['source_priority'] = _sourcePriority;
+    map['country'] = _country;
+    map['category'] = _category;
+    map['language'] = _language;
+    map['ai_tag'] = _aiTag;
+    map['sentiment'] = _sentiment;
+    map['sentiment_stats'] = _sentimentStats;
+    map['ai_region'] = _aiRegion;
+    return map;
+  }
+
+}
